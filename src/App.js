6f4cc9472e710aa5e5e706b10464d48c4f169675
armando-fandango <armando-fandango@users.noreply.github.com>
2023-05-09 05:32:22 +0000

    added arorasim.ai apex domain

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to ARORA Sim
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://arorasim.ai"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          arorasim.ai
         </a>
       </header>
     </div>
