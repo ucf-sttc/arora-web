15ceb5a4294b0c0773ef2d01bc3b9233916720f1
armando-fandango <armando-fandango@users.noreply.github.com>
2023-06-09 23:49:50 +0000

    deploy: 0471fa2eadaf89de2f40da645cd9c973c7d8edbc

---
+++ _next/static/chunks/app/contact/page-b27c08dc8d326dcd.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{6529:function(e,r,t){Promise.resolve().then(t.bind(t,4268))},4268:function(e,r,t){"use strict";t.r(r);var n=t(9953);t(9873),r.default=()=>(0,n.jsx)("main",{className:"flex min-h-screen flex-col items-center justify-between p-24",children:"Contact Dean Read for more information: dreed at ucf dot edu"})},9911:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(9873),o=Symbol.for("react.element"),f=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,t){var n,i={},u=null,a=null;for(n in void 0!==t&&(u=""+t),void 0!==r.key&&(u=""+r.key),void 0!==r.ref&&(a=r.ref),r)f.call(r,n)&&!c.hasOwnProperty(n)&&(i[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===i[n]&&(i[n]=r[n]);return{$$typeof:o,type:e,key:u,ref:a,props:i,_owner:s.current}}r.jsx=i,r.jsxs=i},9953:function(e,r,t){"use strict";e.exports=t(9911)}},function(e){e.O(0,[159,890,744],function(){return e(e.s=6529)}),_N_E=e.O()}]);
