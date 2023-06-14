2b36821054c61d9764f44b7599f8f3ab7ab3f037
Armando Fandango <armando-fandango@users.noreply.github.com>
2023-06-14 13:16:59 +0000

    updated features page and get started page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
 		<main className="flex flex-col items-center justify-start">
 			<div className="flex max-w-5xl">
 				<div className="w-1/2 px-4 justify-center items-center">
-					<p className="mb-2 mt-2">
+					<p className="mb-4 mt-4">
 						ARORA stands for A Realistic Open environment for Rapid Agent
 						training. ARORA is a geospecific simulator for training agents using
 						Reinforcment Learning ARORA is developed to train agents to complete
@@ -15,7 +15,7 @@ const AboutPage = () => {
 						complexities of physics-based movement, vehicle modelling, and a
 						continuous action space. 
             </p>
-            <p className="mb-2 mt-2">
+            <p className="mb-4 mt-4">
             The goal of ARORA is to provide an
 						open-source platform where scientists can explore AI tasks that
 						simulate agents and the real world with high fidelity. ARORA is
@@ -23,7 +23,12 @@ const AboutPage = () => {
 						(i.e., without any graphical interface), and provides a flexible
 						application programming interface (API) with several sensors.
 					</p>
-					
+					<Image
+							src="/images/seg3.png"
+							width={800}
+							height={100}
+							alt="ARORA Sim"
+						/>
 				</div>
 				<div className="w-1/4 px-4 justify-center items-center">
 					<Image
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -2,25 +2,61 @@
 import Image from 'next/image';
 const StartPage = () => {
 	return (
-
 		<main className="flex flex-col items-center justify-start">
-
 			<div className="flex flex-col max-w-5xl">
 				<div className="flex flex-wrap mb-4">
 					<div className="flex flex-col w-1/2">
 						<h2 className="mb-4 mt-4 font-bold text-yellow-400">
-							Introduction
+							Getting started with ARORA
 						</h2>
 						<p className="mb-2 mt-2 text-justify">
 							You can use ARORA in two ways: by downloading the code from the
-							repository and building it yourself or by downloading the binary
-							build.
+							repository and building it yourself with unity or by downloading
+							the binary build. The code repository is avaialable at the
+							following link:{' '}
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
+								href="https://github.com/ucf-sttc/ARORA"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://github.com/ucf-sttc/ARORA
+							</a>
+							, and the binary build is available at the following link:{' '}
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
+								href="https://github.com/ucf-sttc/ARORA/releases"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://github.com/ucf-sttc/ARORA/releases
+							</a>
+							.
 						</p>
+						<p>
+							ARORA Simulator is best when used with navsim, described below, or
+							you can also use it with your own reinforcement learning
+							framework.
+						</p>
+						<h2 className="mb-4 mt-4 font-bold text-yellow-400">
+							Getting started with navsim
+						</h2>
 						<p className="mb-2 mt-2 text-justify">
 							ARORA comes with a python based framework to train reinforcement
-							learnign agents. You can use navsim from the repository below. The
-							documentation for navsim is provided at the following link:{' '}
-							<a className=" text-blue-600 dark:text-blue-500 hover:underline"
+							learnign agents. You can download navsim from the following link:
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
+								href="https://github.com/ucf-sttc/navsim"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://github.com/ucf-sttc/navsim
+							</a>{' '}
+							and find the instructions for quick start in the README or the
+							documentation. The documentation for navsim is provided at the
+							following link:
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
 								href="https://docs.navsim.ai"
 								target="_blank"
 								rel="noopener noreferrer"
@@ -29,9 +65,15 @@ const StartPage = () => {
 							</a>
 						</p>
 					</div>
-					<div className="flex flex-col w-1/2 justify-center px-4">
+					<div className="flex flex-col w-1/2 justify-start px-4">
 						<Image
-							src="/images/home_splash.png"
+							src="/images/seg1.png"
+							width={800}
+							height={100}
+							alt="ARORA Sim"
+						/>
+						<Image
+							src="/images/seg2.png"
 							width={800}
 							height={100}
 							alt="ARORA Sim"
@@ -42,7 +84,8 @@ const StartPage = () => {
 					<h2 className="mb-4 mt-4 font-bold text-yellow-400">Repositories</h2>
 					<ul className="ml-4 mb-2 mt-2 list-disc">
 						<li>
-							<a className=" text-blue-600 dark:text-blue-500 hover:underline"
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
 								href="https://github.com/ucf-sttc/ARORA"
 								target="_blank"
 								rel="noopener noreferrer"
@@ -51,7 +94,8 @@ const StartPage = () => {
 							</a>
 						</li>
 						<li>
-							<a className=" text-blue-600 dark:text-blue-500 hover:underline"
+							<a
+								className=" text-blue-600 dark:text-blue-500 hover:underline"
 								href="https://github.com/ucf-sttc/navsim"
 								target="_blank"
 								rel="noopener noreferrer"
